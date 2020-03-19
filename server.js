9729bfd73bf1ea5a20e66f38252ddae0b9a66dc7
Kittisak <dnjooiopa@gmail.com>
2020-03-18 22:51:40 -0700

    set server

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const socketio = require("socket.io")
+const http = require("http")
+const cors = require("cors")
+
+const PORT = process.env.PORT || 5000
+
+// const router = require("./router")
+
+const app = express()
+//app.use(router)
+app.use(cors())
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on("connection", socket => {
+    console.log("We have a new connection")
+
+
+    socket.on("join", ({ name, room_id }, callback) => {
+
+        /*
+
+        Implement with database
+
+        find room id
+        if (error)
+            
+        */
+
+
+        socket.join(room_id)
+        callback()
+      })
+
+
+    socket.on("sendMessage", ({ message, room_id, name }) => {
+        io.to(room_id).emit("message", { name, text: message })
+    })
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Server starts on port ${PORT}`)
+})
