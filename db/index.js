3e15a8c524541fd381d530f4271f698bfbedd3fe
Kittisak <dnjooiopa@gmail.com>
2020-04-02 20:11:27 +0700

    remove ssl

--- db/index.js
+++ db/index.js
@@ -1,16 +1,20 @@
 const { Pool } = require('pg')
 require("dotenv").config()
 
+const isProduction = process.env.NODE_ENV === 'production'
+
+
+
 const pool = new Pool({
     // user: process.env.USER,
     // host: process.env.HOST,
     // database: process.env.DATABASE,
     // password: process.env.PASSWORD,
     // port: process.env.PORT,
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    connectionString: isProduction ? process.env.DATABASE_URL : process.env.PG_URI
 })
-console.log(process.env.DATABASE_URL)
+
+
 module.exports = {
     query: (text, params) => pool.query(text, params),
 }
