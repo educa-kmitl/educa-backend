cdc3e746753ad6f705546d30336140661f9896c9
Kittisak <dnjooiopa@gmail.com>
2020-04-03 19:25:47 +0700

    add bcrypt for hashing password when register

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const Router = require('express-promise-router')
+const bcrypt = require("bcryptjs")
 const db = require('../db')
 
 const router = new Router()
@@ -6,16 +7,21 @@ const router = new Router()
 router.post("/register", async (req, res) => {
     const { email, name, password, role, profile_icon } = req.body
 
-    if (email && name && password && profile_icon, (role == false || role == true)) {
+    if (email && name && password && profile_icon && (role != undefined)) {
+
         // Check if email already exists
         const checkUser = await db.query("SELECT * FROM users WHERE email=$1", [email])
         if (checkUser.rows.length > 0) return res.status(400).json({ error: "Email already exists" })
+
+        // Hash passwords
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
 
         // If not exists
         const query = {
             name: 'insert-user',
             text: 'INSERT INTO users (email, name, password, profile_icon, role) VALUES ($1, $2, $3, $4, $5)',
-            values: [email, name, password, profile_icon, role],
+            values: [email, name, hashedPassword, profile_icon, role],
         }
         await db.query(query)
 
