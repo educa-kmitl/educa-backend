0615c8c88e99e857a25be50f99f42abea5574f9c
Kittisak <dnjooiopa@gmail.com>
2020-04-04 20:26:59 +0700

    create get resource api

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const auth = require("./auth")
 const users = require('./users')
 const rooms = require("./rooms")
+const resources = require("./resources")
 const dev = require("./dev")
 
 module.exports = app => {
   app.use("/api", auth)
   app.use('/api', users)
   app.use("/api", rooms)
+  app.use("/api", resources)
   app.use("/api/dev", dev)
 }
--- routes/resources.js
+++ routes/resources.js
@@ -4,5 +4,12 @@ const db = require('../db')
 
 const router = new Router()
 
+router.get("/resources", async (req, res) => {
+    const { room_id } = req.headers
+    if (!room_id) return res.status(400).json({ error: "Can't get resource" })
+
+    const { rows } = await db.query("SELECT resource_id, video_url, file_url FROM resources WHERE room_id=$1", [room_id])
+    res.json({ resources: rows })
+})
 
 module.exports = router
