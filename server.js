5945b34fad0e107510ec09e644567561f11b48a6
Kittisak <dnjooiopa@gmail.com>
2020-04-05 20:55:27 +0700

    remove socket.io

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express")
-const socketio = require("socket.io")
-const http = require("http")
 const cors = require("cors")
 
 const PORT = process.env.PORT || 5000
@@ -12,24 +10,6 @@ app.use(express.json())
 const mountRoutes = require('./routes')
 mountRoutes(app)
 
-// SOCKET IO //
-const server = http.createServer(app)
-const io = socketio(server)
-
-io.on("connection", socket => {
-    console.log("We have a new connection")
-
-    socket.on("join", ({ room_id, name }, callback) => {
-        socket.join(room_id)
-        callback()
-    })
-
-    socket.on("sendMessage", ({ message, room_id, name }) => {
-        io.to(room_id).emit("message", { name, text: message })
-    })
-
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server starts on port ${PORT}`)
 })
