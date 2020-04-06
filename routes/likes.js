c5faf197ac1a7aaa1162dbc4f972a456660bc1bf
Kittisak <dnjooiopa@gmail.com>
2020-04-06 12:00:19 +0700

    create api post likes

--- routes/likes.js
+++ routes/likes.js
@@ -11,4 +11,24 @@ router.get("/likes", async (req, res) => {
     res.json({ likes: rows })
 })
 
+router.post("/likes", async (req, res) => {
+    const { room_id, user_id } = req.body
+
+    if (!(room_id && user_id)) return res.status(400).json({ error: "Can't add like" })
+
+    try {
+
+        const { rowCount } = await db.query("INSERT INTO likes (room_id, user_id) VALUES ($1, $2)", [room_id, user_id])
+
+        if (rowCount) {
+            res.json({ success: "You liked this room" })
+        } else {
+            res.status(400).json({ error: "Can't like this room" })
+        }
+    } catch (e) {
+        res.status(400).json({ error: e.detail })
+    }
+})
+
+
 module.exports = router
