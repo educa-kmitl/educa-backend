88a3999769d7e6de14712d71a26dbf3cc53bc620
Kittisak <dnjooiopa@gmail.com>
2020-04-08 09:29:32 +0700

    refactor get followers

--- routes/followers.js
+++ routes/followers.js
@@ -7,16 +7,13 @@ router.get("/followers", async (req, res) => {
     const { user_id } = req.headers
     if (!user_id) return res.status(400).json({ error: "Can't get follower data" })
 
-    const followers = await db.query("SELECT student_id FROM followers WHERE teacher_id=$1", [user_id])
+    const query = `SELECT followers.student_id, users.name, users.profile_icon FROM followers
+                   INNER JOIN users
+                   ON (followers.student_id = users.user_id) AND (followers.teacher_id=${user_id})`
 
-    const followerData = []
-    for (let i = 0; i < followers.rows.length; i++) {
-        const follower = followers.rows[i]
-        const user = await db.query("SELECT name FROM users WHERE user_id=$1", [follower.student_id])
-        followerData.push({ ...follower, ...user.rows[0] })
-    }
+    const { rows: followers } = await db.query(query)
 
-    res.json({ followers: followerData })
+    res.json({ followers })
 })
 
 
